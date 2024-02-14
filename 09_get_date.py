1674420a8a688729ccda35855365c6f94a19de58
smithr1057 <108771978+smithr1057@users.noreply.github.com>
2024-02-15 11:01:24 +1300

    Gets the current date and generates filename

---
+++ 09_get_date.py
@@ -0,0 +1,18 @@
+from datetime import date
+
+# Get today's date
+today = date.today()
+
+# Get dat, month and year as individual strings
+day = today.strftime("%d")
+month = today.strftime("%m")
+year = today.strftime("%y")
+
+heading = f"The current date is {day}/{month}/{year}"
+filename = f"MMF_{year}_{month}_{day}"
+
+# Heading
+print(heading)
+print(f"The file name will be {filename}.txt")
+
+
